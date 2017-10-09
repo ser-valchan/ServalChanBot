adf891ef9b6ec25594ed9ae9732366815eb82ee7
servalchan <kemono_friends@outlook.jp>
2017-10-09 20:59:37 +0900

    きのうついか
    メモ+時間

--- bot.js
+++ bot.js
@@ -4,6 +4,10 @@ var bot = new Eris("MzY2NzcxNjA3MzQ1MTY4Mzg2.DLx6Fw.wgqOWTxnJLABY-69o7MOWf2I9i8"
 var runch = ("310061082574323712")//ようこそ/さよならメッセージ送信先
 var online = ("320493645574963200")//オンライン役職のID
 var NotToDo = new Object
+var memo = new Object
+var username = new Object
+require('date-utils');
+var fs = require('fs');
 /*
 ↑の var doo = new Object 削除しましたがダメでしたら戻してください。
 */
@@ -30,6 +34,29 @@ bot.on("messageCreate", (chat) => {
      //ここまでのif-thenの中身は消してくださってかまいません。
      if(chat.content.match("私は何のフレンズ")) {
        bot.createMessage(chat.channel.id, "あなたの名前は" + chat.author.mention + "だよ！アイコンは...これかな?" + chat.author.avatarURL)
+     }
+     //メモ機能(記録しちゃおうぜてきな)
+     if(chat.content.match(/これを覚えて:/)) {
+       memo = chat.content.replace("これを覚えて:", "")
+       fs.writeFile(chat.author.username + ".txt" , memo , function(err) {
+         console.log(err)
+       })
+       bot.createMessage(chat.channel.id, chat.author.mention + "ちゃんのメモ書き:" + memo + "を保存したよ！");
+     }
+     if(chat.content.match(/ちゃんの言っていたこと/)) {
+       username = chat.content.replace("ちゃんの言っていたこと", "")
+       fs.readFile(username + ".txt" , "utf8" , function(err, text) {
+         console.log(err)
+         bot.createMessage(chat.channel.id, username + "ちゃんが言ってたのは:「" + text + "」だよ！")
+       })
+     }
+     //時間
+     if(chat.content == "今何時?") {
+       date = new Date();
+       hh = date.toFormat("HH24");
+       mi = date.toFormat("MI")
+       ss = date.toFormat("SS")
+       bot.createMessage(chat.channel.id, "今は" + hh + ":" + mi + ":" + ss + "だよ！")
      }
    }
 });
